942451e4e0517ede882529ea9e51401f2ac77b4b
Will <william.fanning1@gmail.com>
2016-08-19 13:36:38 -0500

    completed assignment

--- main.py
+++ main.py
@@ -1,25 +1,68 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+from caesar import encrypt
+
+header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Caesar</title>
+    <style type="text/css">
+        form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+        textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+    </style>
+</head>
+<body>
+"""
+
+footer = """
+</body>
+</html>
+"""
+
+#TODO: write main HTML form
+main_html = """
+<form action="/rot13" method="post">
+    <label>Rotate by:
+        <input type="text" name="rot" value="{0}">
+    </label>
+    <textarea name="text">{1}</textarea>
+    <br>
+    <input type="submit">
+</form>
+"""
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        return self.response.write(*a, **kw)
+
+class Main(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.write(header + main_html.format('0','') + footer)
+
+
+class Rot13(Handler):
+    def post(self):
+        rot = int(self.request.get('rot'))
+        text = self.request.get('text')
+        rotText = encrypt(text, rot)
+        self.write(header + main_html.format(rot, rotText) + footer)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Main),
+    ('/rot13', Rot13),
 ], debug=True)
